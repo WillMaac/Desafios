76e91651efdda7506a7f026037fa5b13a3d26a5b
WillMaac <anderrwil@gmail.com>
2026-01-12 10:01:33 -0300

    first commit

---
+++ desafio.js
@@ -0,0 +1,28 @@
+function desconto (totalCompra){
+if(typeof totalCompra !== "number" || totalCompra <= 0){
+    return "Valor inválido";
+}
+
+let valorDesconto = 0;
+
+if(totalCompra >= 500){
+     valorDesconto = totalCompra * 0.20
+}
+else if(totalCompra >= 300){
+ valorDesconto = totalCompra * 0.10
+}
+
+
+
+return {
+    totalOriginal: totalCompra,
+
+    desconto: valorDesconto,
+
+    totalFinal: totalCompra - valorDesconto
+}
+}
+
+console.log(desconto(550));
+console.log(desconto(450));
+console.log(desconto(200));
